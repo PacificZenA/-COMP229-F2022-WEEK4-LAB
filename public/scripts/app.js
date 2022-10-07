b27760d61127e6ed2731e9ebf47f51a97156338d
Alex Tang <alextoronto66@gmail.com>
2022-10-07 15:15:56 -0400

    Initial Week4 Code

---
+++ public/scripts/app.js
@@ -0,0 +1,17 @@
+/* File name: app.js
+Student Name: Alex Tang
+StudentID: 301238850
+Date: Oct/5/2022
+*/
+
+(function () {
+  function Start() {
+    console.log("App Started");
+  }
+  window.addEventListener("load", Start);
+})();
+
+//Function for redirecting to home page
+function redirectToHome() {
+  window.location.href = "http://localhost:3000/home";
+}
